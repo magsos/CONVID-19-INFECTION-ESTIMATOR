3281d621fa05082a4bcca810a9cfd5bdbab09265
magsos <magsosmj@gmail.com>
2020-04-08 18:21:51 +0100

    start building estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,33 @@
-const covid19ImpactEstimator = (data) => data;
+input = {
+    region: {
+    name: "Africa",
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+   }
 
-export default covid19ImpactEstimator;
+const Infectedpips = (input, value ) => input.reportedCases * value 
+
+const covid19ImpactEstimator = (data) => {
+    const input = data;
+    return {
+        data: input, 
+        impact: {
+            currentlyInfected: Infectedpips(input, 10)        
+        },
+        severeImpact: {
+            currentlyInfected: Infectedpips(input, 50)
+        }
+    };
+};
+
+console.log(covid19ImpactEstimator(input))
+module.exports = {
+    covid19ImpactEstimator
+}
